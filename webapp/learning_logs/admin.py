c8e6512a04348b39609d749df1115db8e3bfeee7
PC-DUSS <pierrecharles.dussault@gmail.com>
2021-05-19 12:42:12 -0400

    Separate heroku webapp to a different repository.

--- webapp/learning_logs/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-"""The dot in front of 'models' tells Django to look for 'models.py' in the
-same directory as 'admin.py'."""
-from .models import Topic, Entry
-admin.site.register(Topic)
-admin.site.register(Entry)
